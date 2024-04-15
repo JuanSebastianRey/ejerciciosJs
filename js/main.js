141d1fbc132612a4ef2c787ae8b2741d68d29662
JuanSebastianRey <149455429+JuanSebastianRey@users.noreply.github.com>
2024-04-15 12:51:47 -0500

    feat: :construction: Ejercicio contraseña
    Se creo la solucion parcial del primer ejercicio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 import prompt from "prompt-sync"
+import contraseñaValida from "./module/contraseña.js"
 let input = prompt();
-let n = input('How many more times? ')
-console.log(n);
+let opc = undefined;
+do{
+    let data = input("Ingrese la contraseña: ")
+    console.log(contraseñaValida(data));
+    console.log(`1. Repetir la validacion\n0. Terminar`);
+    opc = Number(input())
+}while(opc)
